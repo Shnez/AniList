64e24b57e31fd1fe0c8a3d4fa8d7d682909163b9
Shnez <enriqueodaris@gmail.com>
2023-06-29 14:33:46 -0400

    Initial code for AniList project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Header from "./components/Header"
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
+import Navbar from './components/Navbar';
 
 function App() {
+  const [animeList, SetAnimeList] = useState([]);
+  const [topAnime, SetTopAnime] = useState([]);
+  const [search, SetSearch] = useState("");
+
+  const GetTopAnime = async () => {
+      const temp = await fetch(`https://api.jikan.moe/v4/top/anime`,
+      {
+        params: {
+          filter: "bypopularity",
+          page: 1,
+        }
+      })
+        .then(res => res.json());
+
+      SetTopAnime(temp.data.slice(0, 5))
+  }
+
+  const HandleSearch = e => {
+    e.preventDefault();
+
+    FetchAnime(search)
+  }
+
+  const FetchAnime = async (query) => {
+    const temp = await fetch(`https://api.jikan.moe/v4/anime?q=${query}
+    &sort=asc&limit=10`)
+      .then(res => res.json());
+    SetAnimeList(temp.data);
+  }
+
+  useEffect(() => {
+      GetTopAnime()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+		  <Header />
+      <div className="content-wrap">
+          <Sidebar 
+            topAnime=
+            {topAnime} />
+          <MainContent 
+              HandleSearch = {HandleSearch}
+              search={search}
+              SetSearch={SetSearch}
+              animeList={animeList}/>
+      </div>
     </div>
   );
 }
